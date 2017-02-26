82e26ab530f9e49b65682e95eb66a3d60f797636
dannyzhou98 <daniel.zhou2012@gmail.com>
2017-02-26 06:14:44 -0500

    fixed AddEvent

--- app/src/main/java/com/a2017/hackers/hacktcnj2017/MapFragment.java
+++ app/src/main/java/com/a2017/hackers/hacktcnj2017/MapFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -20,7 +21,6 @@ import android.view.ViewGroup;
 public class MapFragment extends Fragment {
 
 
-    GoogleMap mMap;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
